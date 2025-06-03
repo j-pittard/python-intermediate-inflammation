aea2f9787a067f05c46a8bcb9c21cd9be2aae6b7
James Pittard <james.pittard@ukaea.uk>
2025-06-03 13:40:13 +0100

    Forgot to hit save..

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import numpy.testing as npt
+import pytest
 
 from inflammation.models import daily_mean, daily_max, daily_min
 
@@ -52,3 +53,8 @@ def test_daily_min_integers():
     # Need to use Numpy testing functions to compare arrays
     npt.assert_array_equal(daily_min(test_input), test_result)
 
+def test_daily_min_string():
+    """Test for TypeError when passing strings"""
+
+    with pytest.raises(TypeError):
+        error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
